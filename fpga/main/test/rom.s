d98505fdb94a06f4c9ac3aec30ad9fa0c385620d
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2019-11-03 08:45:42 +0100

    Fixed bug in Makefile.

--- fpga/main/test/rom.s
+++ fpga/main/test/rom.s
@@ -6,71 +6,71 @@ main:
    LDA #$00    ; Select address port 0
    STA $9F25
 
-   LDA #$80    ; Set address to 0x02080 and increment to 1.
+   LDA #$50    ; Set address to 0x02050 and increment to 1.
    STA $9F20
    LDA #$20
    STA $9F21
    LDA #$10
    STA $9F22
 
-   LDA #$00    ; Write to 0x02080
+   LDA #$00    ; Write to 0x02050
    STA $9F23
-   LDA #$44    ; Write to 0x02081
-   STA $9F23
-
-   LDA #$00    ; Write to 0x02082
-   STA $9F23
-   LDA #$66    ; Write to 0x02083
+   LDA #$44    ; Write to 0x02051
    STA $9F23
 
-   LDA #$00    ; Write to 0x02084
+   LDA #$00    ; Write to 0x02052
    STA $9F23
-   LDA #$66    ; Write to 0x02085
-   STA $9F23
-
-   LDA #$00    ; Write to 0x02086
-   STA $9F23
-   LDA #$66    ; Write to 0x02087
+   LDA #$66    ; Write to 0x02053
    STA $9F23
 
-   LDA #$00    ; Write to 0x02088
+   LDA #$00    ; Write to 0x02054
    STA $9F23
-   LDA #$44    ; Write to 0x02089
+   LDA #$66    ; Write to 0x02055
+   STA $9F23
+
+   LDA #$00    ; Write to 0x02056
+   STA $9F23
+   LDA #$66    ; Write to 0x02057
+   STA $9F23
+
+   LDA #$00    ; Write to 0x02058
+   STA $9F23
+   LDA #$44    ; Write to 0x02059
    STA $9F23
 
    NOP
    NOP
 
-   LDA #$80    ; Set address to 0x02180 and increment to 1.
+   LDA #$50    ; Set address to 0x02150 and increment to 1.
    STA $9F20
    LDA #$21
    STA $9F21
    LDA #$10
    STA $9F22
 
-   LDA #$00    ; Write to 0x02180
+   LDA #$00    ; Write to 0x02150
    STA $9F23
-   LDA #$66    ; Write to 0x02181
-   STA $9F23
-
-   LDA #$00    ; Write to 0x02182
-   STA $9F23
-   LDA #$44    ; Write to 0x02183
+   LDA #$66    ; Write to 0x02151
    STA $9F23
 
-   LDA #$00    ; Write to 0x02184
+   LDA #$00    ; Write to 0x02152
    STA $9F23
-   LDA #$66    ; Write to 0x02185
-   STA $9F23
-
-   LDA #$00    ; Write to 0x02186
-   STA $9F23
-   LDA #$44    ; Write to 0x02187
+   LDA #$44    ; Write to 0x02153
    STA $9F23
 
-   LDA #$00    ; Write to 0x02188
+   LDA #$00    ; Write to 0x02154
    STA $9F23
-   LDA #$66    ; Write to 0x02189
+   LDA #$66    ; Write to 0x02155
+   STA $9F23
+
+   LDA #$00    ; Write to 0x02156
+   STA $9F23
+   LDA #$44    ; Write to 0x02157
+   STA $9F23
+
+   LDA #$00    ; Write to 0x02158
+   STA $9F23
+   LDA #$66    ; Write to 0x02159
    STA $9F23
 
    NOP
